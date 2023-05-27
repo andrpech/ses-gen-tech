9607c640c679749ebfa7c250a879d251f876061e
andrpech <andrii.pechyborshch@gmail.com>
2023-05-27 12:44:51 +0300

    feat: add binance rate api call

--- main.go
+++ main.go
@@ -1,23 +1,84 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
+	"log"
+	"net/http"
 
 	"github.com/labstack/echo/v4"
 )
 
-const port string = ":8080"
+const (
+	port    string = ":8080"
+	apiPath string = "/api"
+)
 
+const symbol string = "BTCUAH"
+
+type RateResponse struct {
+	Symbol string `json:"symbol"`
+	Price  string `json:"price"`
+}
+
+type ErrorResponse struct {
+	Code int    `json:"code"`
+	Msg  string `json:"msg"`
+}
 
 func main() {
 	fmt.Println("Hello, gen tech!")
 
-	s := echo.New()
+	e := echo.New()
 
-	s.GET("/kenobi", func(c echo.Context) error { return c.String(200, "Hello there") })
+	apiGroup := e.Group(apiPath)
+
+	apiGroup.GET("/kenobi", healthCheck)
+
+	apiGroup.GET("/rate", getRate)
 
 	fmt.Println("Server started")
+
 	defer fmt.Println("Server stopped")
 
-	s.Start(port)
+	e.Start(port)
+}
+
+func healthCheck(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello there")
+}
+
+func getRate(c echo.Context) error {
+	url := `https://api.binance.com/api/v3/ticker/price?symbol=` + symbol
+
+	resp, err := http.Get(url)
+	if err != nil {
+		return c.String(http.StatusInternalServerError, `Error occurred: `+err.Error())
+	}
+	defer resp.Body.Close()
+
+	log.Println("Response status code:", resp.StatusCode)
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return c.String(http.StatusInternalServerError, `Error occurred: `+err.Error())
+	}
+
+	// Check for error response
+	var errResp ErrorResponse
+	if err := json.Unmarshal(body, &errResp); err == nil && errResp.Code != 0 {
+		return c.JSON(http.StatusInternalServerError, map[string]string{"message": `Error from Binance API: ` + errResp.Msg})
+	}
+
+	var rateResp RateResponse
+	if err := json.Unmarshal(body, &rateResp); err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]string{
+			"message": `Failed to parse response from Binance: ` + err.Error(),
+		})
+	}
+
+	log.Println(string(body))
+
+	return c.JSONBlob(http.StatusOK, body)
 }
