5b805111907c7c16ba6503a27bc3f26a4a0e624f
andrpech <andrii.pechyborshch@gmail.com>
2023-05-27 11:12:35 +0300

    feat: add echo server

--- main.go
+++ main.go
@@ -2,8 +2,22 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/labstack/echo/v4"
 )
 
+const port string = ":8080"
+
+
 func main() {
-	fmt.Println("Hello, gen tech")
+	fmt.Println("Hello, gen tech!")
+
+	s := echo.New()
+
+	s.GET("/kenobi", func(c echo.Context) error { return c.String(200, "Hello there") })
+
+	fmt.Println("Server started")
+	defer fmt.Println("Server stopped")
+
+	s.Start(port)
 }
